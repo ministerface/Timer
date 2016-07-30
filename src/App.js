f6383926a3884c4d6afa3b1b50dd4ba7fb406f03
Minter Face <ministerface@gmail.com>
2016-07-30 14:49:35 +0300

    new

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+import React, {
+    Component
+} from 'react';
 import ListOfTimers from './ListOfTimer';
 import AddTimer from './AddTimer';
 import { v4 } from 'node-uuid';
@@ -6,112 +8,133 @@ import { v4 } from 'node-uuid';
 require('./styles/main.scss');
 
 
-
-
 export default class App extends Component {
-    constructor(props){
-      super(props);
-      this.state = {
-        counters: []
-      }
+    constructor(props) {
+        super(props);
+        this.state = {
+            counters: []
+        }
+    }
+
+    findIndex(id) {
+        const { counters } = this.state;
+        return counters.findIndex((el) => el.id === id);
     }
 
 
-    tick = (id) => {
-      const { counters } = this.state;
-      const index = counters.findIndex( (el) => el.id === this.id );
 
-      counters[index].time = this.state.counters[index].time + 1;
-      counters[index].price = this.state.counters[index].price + 100
+    tick = (index) => {
+        const {
+            counters
+        } = this.state;
+        counters[index].time = this.state.counters[index].time + 1;
+        counters[index].price = this.state.counters[index].price + 100
 
-      this.setState(
-        {counters: counters}
-      );
+        this.setState({
+            counters: counters
+        });
     }
 
 
-    pause = (id) =>{
-      const { counters } = this.state;
-      const index = counters.findIndex( (el) => el.id === id );
+    pause = (id) => {
+        const {
+            counters
+        } = this.state;
+        const index = counters.findIndex((el) => el.id === id);
 
-      counters[index].active.start = false;
+        counters[index].active.start = false;
 
 
-      clearInterval(counters[index].active.time);
+        clearInterval(counters[index].active.time);
     }
 
-      start = (id) => {
-          const { counters } = this.state;
+    start = (id) => {
+        const {
+            counters
+        } = this.state;
 
-      counters.map(function(counter, i) {
-              if (id !== counter.id) {
+
+        counters.map(function(counter, i) {
+            if (id !== counter.id) {
                 clearInterval(counter.active.time);
-              }
+            }
         });
 
+        const index = this.findIndex(id);
 
-
-
-        const index = counters.findIndex( (el) => el.id === id );
-
-        this.id = id;
         counters[index].active.start = true;
-        counters[index].active.time = setInterval(this.tick, 1000);
+        counters[index].active.time = setInterval(() => {
+            this.tick(index);
+        }, 1000);
 
-      }
+    }
 
-      reset = (id) => {
+    reset = (id) => {
 
-        const { counters } = this.state;
-        const index = counters.findIndex( (el) => el.id === this.id );
+        const {
+            counters
+        } = this.state;
+        const index = counters.findIndex((el) => el.id === this.id);
 
         counters[index].time = 0;
         counters[index].price = 0;
 
-        this.setState(
-          {counters: counters}
-        );
+        this.setState({
+            counters: counters
+        });
 
-      }
+    }
 
 
 
     addTimer = () => {
 
-        const { counters } = this.state;
+        const {
+            counters
+        } = this.state;
         counters.unshift({
-          id: v4(),
-          name:'Новый счетчик',
-          time:0,
-          price:0,
-          active: {
-            start:false,
-            time: null
-          }
+            id: v4(),
+            name: 'Новый счетчик',
+            time: 0,
+            price: 0,
+            active: {
+                start: false,
+                time: null
+            }
         });
 
-        this.setState(
-          {counters: counters}
-        );
+        this.setState({
+            counters: counters
+        });
     }
 
 
 
 
-    render (){
-      const actions = {
-        start: this.start,
-        pause: this.pause,
-        reset: this.reset
-      }
+    render() {
+        const actions = {
+            start: this.start,
+            pause: this.pause,
+            reset: this.reset
+        }
 
 
-      return(
-        <div className="timer-app">
-          <ListOfTimers action={actions} counters={this.state.counters}/>
-          <AddTimer action={this.addTimer}/>
-        </div>
-      )
+        return ( <
+            div className = "timer-app" >
+            <
+            ListOfTimers action = {
+                actions
+            }
+            counters = {
+                this.state.counters
+            }
+            /> <
+            AddTimer action = {
+                this.addTimer
+            }
+            /> < /
+            div >
+        )
     };
 
 
@@ -119,6 +142,6 @@ export default class App extends Component {
 
 
 
-App.getDefaultProps ={
-  start: Date.now(),
+App.getDefaultProps = {
+    start: Date.now(),
 }
